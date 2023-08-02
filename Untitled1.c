9467c7f3b68b718e099e23ab33b84653a23801c9
vishwaskaushik <141205395+vishwaskaushik@users.noreply.github.com>
2023-08-02 10:33:37 +0530

    Add files via upload

---
+++ Untitled1.c
@@ -0,0 +1,15 @@
+# include <stdio.h>
+int main()
+{
+    int a=0;
+    int b=0;
+    int add;
+    printf("enter two integers \n");
+    scanf("%d",&a);
+    scanf("%d",&b);
+    add= a+b;
+    printf("sum = %d",add);
+    return 0;
+
+
+}
